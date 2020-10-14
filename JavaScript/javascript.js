5584f4c9c24983eb5fdf9998e5cf329c8dffa19e
Alberto López Hernández <2019099@CALASANZSALAMANCA.local>
2020-10-14 12:59:05 +0200

    first commit

---
+++ JavaScript/javascript.js
@@ -0,0 +1,32 @@
+var cantidadFruta = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+var precioFruta = new Array(1.1, 1.2 , 1.3, 1.4 ,1.5, 1.6, 1.7, 1.8, 1.9, 1.99);
+var nombreFruta = new Array("Pera","Manzana", "Uva", "Melocoton", "Sandia", "Melón", "Platanos", "Kiwi", "Fresa", "Naranja" );
+
+var precioTotal = 0;
+var precioMedio = 0
+
+function compra(frutas){
+    var  i ;
+    for ( i  =  0 ;  i  <  frutitas . length ; i ++ ) {
+        if ( frutitas [ i ]  ==  frutas ) {
+            cantidad [ i ] + = 1 ;
+            kilos [ i ] + = 1 ;
+            romper ;
+        }
+    }
+}
+
+function  enviar( ) {
+
+
+    function enviar(){
+        for(var 1 = 0; i < 10; i++){
+            if(cantidadFruta[i] i=0){
+                for(precioFruta.lenght; j++){
+                    precioTotal!+=parseFloat(cantidadFruta)
+                }
+            }
+        }
+    }
+  
+}
