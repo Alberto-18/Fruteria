2d063e7fd25bf3afd011c0b572b57c2cf4474170
Alberto-18 <66730701+Alberto-18@users.noreply.github.com>
2020-11-17 12:01:08 +0100

    Add files via upload

--- JavaScript/javascript.js
+++ JavaScript/javascript.js
@@ -1,9 +1,171 @@
+// Prueba -----------------------------------------------------------------------------------------------------------------------------------
+window.onload = function () {
+    // Variables
+    let baseDeDatos = [
+        {
+            id: 1,
+            nombre: 'Patata',
+            precio: 1,
+            imagen: 'https://source.unsplash.com/random/500x500/?potato&sig=1'
+        },
+        {
+            id: 2,
+            nombre: 'Cebolla',
+            precio: 1.2,
+            imagen: 'https://source.unsplash.com/random/500x500/?onion&sig=2'
+        },
+        {
+            id: 3,
+            nombre: 'Calabacin',
+            precio: 2.1,
+            imagen: 'https://source.unsplash.com/random/500x500/?zucchini&sig=3'
+        },
+        {
+            id: 4,
+            nombre: 'Fresas',
+            precio: 0.6,
+            imagen: 'https://source.unsplash.com/random/500x500/?burrs&sig=4'
+        }
+
+    ]
+    let $items = document.querySelector('#items');
+    let carrito = [];
+    let total = 0;
+    let $carrito = document.querySelector('#carrito');
+    let $total = document.querySelector('#total');
+    let $botonVaciar = document.querySelector('#boton-vaciar');
+
+    // Funciones
+    function renderItems() {
+        for (let info of baseDeDatos) {
+            // Estructura
+            let miNodo = document.createElement('div');
+            miNodo.classList.add('card', 'col-sm-4');
+            // Body
+            let miNodoCardBody = document.createElement('div');
+            miNodoCardBody.classList.add('card-body');
+            // Titulo
+            let miNodoTitle = document.createElement('h5');
+            miNodoTitle.classList.add('card-title');
+            miNodoTitle.textContent = info['nombre'];
+            // Imagen
+            let miNodoImagen = document.createElement('img');
+            miNodoImagen.classList.add('img-fluid');
+            miNodoImagen.setAttribute('src', info['imagen']);
+            // Precio
+            let miNodoPrecio = document.createElement('p');
+            miNodoPrecio.classList.add('card-text');
+            miNodoPrecio.textContent = info['precio'] + '€';
+            // Boton 
+            let miNodoBoton = document.createElement('button');
+            miNodoBoton.classList.add('btn', 'btn-primary');
+            miNodoBoton.textContent = '+';
+            miNodoBoton.setAttribute('marcador', info['id']);
+            miNodoBoton.addEventListener('click', anyadirCarrito);
+            // Insertamos
+            miNodoCardBody.appendChild(miNodoImagen);
+            miNodoCardBody.appendChild(miNodoTitle);
+            miNodoCardBody.appendChild(miNodoPrecio);
+            miNodoCardBody.appendChild(miNodoBoton);
+            miNodo.appendChild(miNodoCardBody);
+            $items.appendChild(miNodo);
+        }
+    }
+
+    function anyadirCarrito () {
+        // Anyadimos el Nodo a nuestro carrito
+        carrito.push(this.getAttribute('marcador'))
+        // Calculo el total
+        calcularTotal();
+        // Renderizamos el carrito 
+        renderizarCarrito();
+    }
+
+    function renderizarCarrito() {
+        // Vaciamos todo el html
+        $carrito.textContent = '';
+        // Quitamos los duplicados
+        let carritoSinDuplicados = [...new Set(carrito)];
+        // Generamos los Nodos a partir de carrito
+        carritoSinDuplicados.forEach(function (item, indice) {
+            // Obtenemos el item que necesitamos de la variable base de datos
+            let miItem = baseDeDatos.filter(function(itemBaseDatos) {
+                return itemBaseDatos['id'] == item;
+            });
+            // Cuenta el número de veces que se repite el producto
+            let numeroUnidadesItem = carrito.reduce(function (total, itemId) {
+                return itemId === item ? total += 1 : total;
+            }, 0);
+            // Creamos el nodo del item del carrito
+            let miNodo = document.createElement('li');
+            miNodo.classList.add('list-group-item', 'text-right', 'mx-2');
+            miNodo.textContent = `${numeroUnidadesItem} x ${miItem[0]['nombre']} - ${miItem[0]['precio']}€`;
+            // Boton de borrar
+            let miBoton = document.createElement('button');
+            miBoton.classList.add('btn', 'btn-danger', 'mx-5');
+            miBoton.textContent = 'X';
+            miBoton.style.marginLeft = '1rem';
+            miBoton.setAttribute('item', item);
+            miBoton.addEventListener('click', borrarItemCarrito);
+            // Mezclamos nodos
+            miNodo.appendChild(miBoton);
+            $carrito.appendChild(miNodo);
+        })
+    }
+
+    function borrarItemCarrito() {
+        console.log()
+        // Obtenemos el producto ID que hay en el boton pulsado
+        let id = this.getAttribute('item');
+        // Borramos todos los productos
+        carrito = carrito.filter(function (carritoId) {
+            return carritoId !== id;
+        });
+        // volvemos a renderizar
+        renderizarCarrito();
+        // Calculamos de nuevo el precio
+        calcularTotal();
+    }
+
+    function calcularTotal() {
+        // Limpiamos precio anterior
+        total = 0;
+        // Recorremos el array del carrito
+        for (let item of carrito) {
+            // De cada elemento obtenemos su precio
+            let miItem = baseDeDatos.filter(function(itemBaseDatos) {
+                return itemBaseDatos['id'] == item;
+            });
+            total = total + miItem[0]['precio'];
+        }
+        // Formateamos el total para que solo tenga dos decimales
+        let totalDosDecimales = total.toFixed(2);
+        // Renderizamos el precio en el HTML
+        $total.textContent = totalDosDecimales;
+    }
+
+    function vaciarCarrito() {
+        // Limpiamos los productos guardados
+        carrito = [];
+        // Renderizamos los cambios
+        renderizarCarrito();
+        calcularTotal();
+    }
+
+    // Eventos
+    $botonVaciar.addEventListener('click', vaciarCarrito);
+
+    // Inicio
+    renderItems();
+} 
+
+//-----------------------------------------------------------------------------------------------------------------------------------
 var cantidadFruta = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 var precioFruta = new Array(1.1, 1.2 , 1.3, 1.4 ,1.5, 1.6, 1.7, 1.8, 1.9, 1.99);
 var nombreFruta = new Array("Pera","Manzana", "Uva", "Melocoton", "Sandia", "Melón", "Platanos", "Kiwi", "Fresa", "Naranja" );
 
 var precioTotal = 0;
-var precioMedio = 0
+var precioMedio = 0;
 
 
 class Fruta{
@@ -62,7 +224,6 @@ let Naranja = new FrutasVerano(0,1.99,"Naranja","de proximidad","Andalucía");
 function compra(frutas){
     let derecha = document.getElementById("parteDerecha");
     let texto = document.createTextNode("Se han añadido " + kilos + " kilos de " + frutas + "\n");
-    let parrafo = document.createElement("p");
     parrafo.appendChild(texto);
 
 
@@ -144,6 +305,8 @@ function funcion(Fruta){
         texto += Manzana.getNombre() + " Fruta de Invernal " + Manzana.getProximidad();
         precioTotal += Manzana.getKilos()*Manzana.getPrecio();
         contador++;
+
+
     }
 
     if(Uva.getKilos() != 0){
@@ -163,7 +326,7 @@ function funcion(Fruta){
         precioTotal += Sandia.getKilos()*Sandia.getPrecio();
         contador++;
     }
-''
+
     if(Melón.getKilos() != 0){
         texto += Melón.getNombre() + "Fruta de Verano  " + Melón.getProximidad();
         precioTotal += Melón.getKilos()*Melón.getPrecio();
@@ -195,4 +358,3 @@ function funcion(Fruta){
     }
 
 }
-
